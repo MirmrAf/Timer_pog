ff76bebd34c4f067ffc706f8f27baa49929b9c18
Bodya <mimra6232@gmail.com>
2025-09-11 08:48:49 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Timer_pog
+{
+    partial class LogOutTimer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogOutTimer));
+            timer_label = new Label();
+            hours_mtb = new MaskedTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            minutes_mtb = new MaskedTextBox();
+            label3 = new Label();
+            seconds_mtb = new MaskedTextBox();
+            start_button = new Button();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // timer_label
+            // 
+            timer_label.Font = new Font("Segoe UI", 18F);
+            timer_label.Location = new Point(136, 20);
+            timer_label.Name = "timer_label";
+            timer_label.Size = new Size(257, 36);
+            timer_label.TabIndex = 0;
+            timer_label.Text = "time: 0:0:0";
+            // 
+            // hours_mtb
+            // 
+            hours_mtb.BorderStyle = BorderStyle.FixedSingle;
+            hours_mtb.Location = new Point(12, 91);
+            hours_mtb.Mask = "99";
+            hours_mtb.Name = "hours_mtb";
+            hours_mtb.Size = new Size(49, 23);
+            hours_mtb.TabIndex = 1;
+            hours_mtb.MaskInputRejected += hours_mtb_MaskInputRejected;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 2;
+            label1.Text = "hours";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(83, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 4;
+            label2.Text = "minutes";
+            // 
+            // minutes_mtb
+            // 
+            minutes_mtb.BorderStyle = BorderStyle.FixedSingle;
+            minutes_mtb.Location = new Point(83, 91);
+            minutes_mtb.Mask = "99";
+            minutes_mtb.Name = "minutes_mtb";
+            minutes_mtb.Size = new Size(49, 23);
+            minutes_mtb.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(158, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 15);
+            label3.TabIndex = 6;
+            label3.Text = "seconds";
+            // 
+            // seconds_mtb
+            // 
+            seconds_mtb.BorderStyle = BorderStyle.FixedSingle;
+            seconds_mtb.Location = new Point(158, 91);
+            seconds_mtb.Mask = "99";
+            seconds_mtb.Name = "seconds_mtb";
+            seconds_mtb.Size = new Size(49, 23);
+            seconds_mtb.TabIndex = 5;
+            // 
+            // start_button
+            // 
+            start_button.Location = new Point(12, 20);
+            start_button.Name = "start_button";
+            start_button.Size = new Size(75, 23);
+            start_button.TabIndex = 7;
+            start_button.Text = "start";
+            start_button.UseVisualStyleBackColor = true;
+            start_button.Click += start_button_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "stop";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 130);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "some sounds?";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(158, 126);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 10;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(220, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Key select";
+            // 
+            // LogOutTimer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(304, 161);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(start_button);
+            Controls.Add(label3);
+            Controls.Add(seconds_mtb);
+            Controls.Add(label2);
+            Controls.Add(minutes_mtb);
+            Controls.Add(label1);
+            Controls.Add(hours_mtb);
+            Controls.Add(timer_label);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "LogOutTimer";
+            RightToLeftLayout = true;
+            Text = "Log Out Timer!";
+            TopMost = true;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label timer_label;
+        private MaskedTextBox hours_mtb;
+        private Label label1;
+        private Label label2;
+        private MaskedTextBox minutes_mtb;
+        private Label label3;
+        private MaskedTextBox seconds_mtb;
+        private Button start_button;
+        private Button button1;
+        private CheckBox checkBox1;
+        private ComboBox comboBox1;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using WindowsInput;
+using WindowsInput.Native;
+
+
+namespace Timer_pog
+{
+
+
+
+    public partial class LogOutTimer : Form
+    {
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        int all_time = 0;
+        int h = 0; // Для лейбла
+        int m = 0; // Для лейбла 
+        int s = 0; // Для лейбла
+        SoundPlayer AllOk;
+        SoundPlayer Bad;
+        int counter = 0;
+        bool timer_is_stoped = true;
+        InputSimulator InSim = new InputSimulator();
+        System.Windows.Forms.Timer timer1;
+        VirtualKeyCode SelectedKeyCode;
+
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+
+        public LogOutTimer()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 200; i++) 
+            {
+                FillComboBoxWithFilteredKeys();
+            }
+
+        }
+        private void FillComboBoxWithFilteredKeys()
+        {
+            // Исключаем некоторые значения, которые обычно не нужны
+            var excludedKeys = new[]
+            {
+            VirtualKeyCode.NONAME,
+            VirtualKeyCode.OEM_1,
+            VirtualKeyCode.OEM_2,
+            VirtualKeyCode.OEM_3,
+            VirtualKeyCode.OEM_4,
+            VirtualKeyCode.OEM_5,
+            VirtualKeyCode.OEM_6,
+            VirtualKeyCode.OEM_7,
+            VirtualKeyCode.OEM_8,
+            VirtualKeyCode.OEM_102,
+            VirtualKeyCode.OEM_CLEAR,
+            VirtualKeyCode.OEM_COMMA,
+            VirtualKeyCode.OEM_PERIOD,
+            VirtualKeyCode.OEM_PLUS,
+            VirtualKeyCode.OEM_MINUS,
+            VirtualKeyCode.VOLUME_DOWN,
+            VirtualKeyCode.VOLUME_UP,
+            VirtualKeyCode.VOLUME_MUTE,
+            VirtualKeyCode.BROWSER_BACK,
+            VirtualKeyCode.BROWSER_FORWARD,
+            VirtualKeyCode.BROWSER_STOP,
+            VirtualKeyCode.BROWSER_FAVORITES,
+            VirtualKeyCode.BROWSER_HOME,
+            VirtualKeyCode.BROWSER_REFRESH,
+            VirtualKeyCode.BROWSER_SEARCH,
+            VirtualKeyCode.XBUTTON1,
+            VirtualKeyCode.XBUTTON2,
+            VirtualKeyCode.ZOOM,
+            VirtualKeyCode.F13,
+            VirtualKeyCode.F14,
+            VirtualKeyCode.F15,
+            VirtualKeyCode.F16,
+            VirtualKeyCode.F17,
+            VirtualKeyCode.F18,
+            VirtualKeyCode.F19,
+            VirtualKeyCode.F20,
+            VirtualKeyCode.F21,
+            VirtualKeyCode.F22,
+            VirtualKeyCode.F23,
+            VirtualKeyCode.F24,
+
+            // добавь другие ненужные значения
+        };
+
+            var keyCodes = Enum.GetValues(typeof(VirtualKeyCode))
+                              .Cast<VirtualKeyCode>()
+                              .Where(k => !excludedKeys.Contains(k))
+                              .OrderBy(k => k.ToString());
+
+            foreach (var keyCode in keyCodes)
+            {
+                comboBox1.Items.Add(keyCode);
+            }
+            
+            comboBox1.SelectedIndex = 24;
+            SelectedKeyCode = (VirtualKeyCode)comboBox1.SelectedItem;
+
+        }
+
+        private void hours_mtb_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (timer_is_stoped == true)
+            {
+                if (seconds_mtb.Text != null | minutes_mtb.Text != null | hours_mtb.Text != null)
+                {
+                    bool okay_s = false;
+                    bool okay_m = false;
+                    bool okay_h = false;
+
+                    try
+                    {
+                        seconds = int.Parse(seconds_mtb.Text);
+                        okay_s = true;
+                    }
+                    catch { okay_s = false; }
+                    try
+                    {
+                        minutes = int.Parse(minutes_mtb.Text);
+                        okay_m = true;
+                    }
+                    catch { okay_m = false; }
+                    try
+                    {
+                        hours = int.Parse(hours_mtb.Text);
+                        okay_h = true;
+                    }
+                    catch { okay_h = false; }
+                    if (okay_s | okay_m | okay_h)
+                    {
+                        timer_is_stoped = false;
+                        Debug.WriteLine(seconds);
+                        Debug.WriteLine(minutes);
+                        Debug.WriteLine(hours);
+                        if (seconds <= 60 & minutes <= 60 & hours <= 24)
+                        {
+                            all_time = hours * 60 * 60 + minutes * 60 + seconds;
+                            timer1 = new System.Windows.Forms.Timer();
+                            timer1.Interval = 1000; // Установите интервал в 1000 миллисекунд (1 секунда)
+                            timer1.Tick += timer_tick;
+                            timer1.Start();
+                        }
+                        else
+                        {
+                            Bad?.Play();
+                            MessageBox.Show("Wrong time!", "Error", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        }
+                    }
+                    else
+                    {
+                        Bad?.Play();
+                        MessageBox.Show("Wrong time!", "Error", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    }
+
+
+                }
+                else
+                {
+                    Bad?.Play();
+                    MessageBox.Show("Wrong time!", "Error", MessageBoxButtons.OK, MessageBoxIcon.None);
+                }
+            }
+
+
+
+        }
+        void timer_tick(object sender, EventArgs e)
+        {
+
+            Debug.WriteLine(all_time);
+            counter++;
+            s++;
+            if (s >= 59)
+            {
+                s = 0;
+                m++;
+            }
+            if (m == 59)
+            {
+                h++;
+            }
+            timer_label.Text = $"time: {h}:{m}:{s}";
+            if (counter == all_time)
+            {
+
+                timer_is_stoped = true;
+                timer1.Stop();
+                timer1 = null;
+                seconds_mtb.Clear();
+                minutes_mtb.Clear();
+                hours_mtb.Clear();
+                Debug.Write("Time has passed");
+                //LockWorkStation();
+                InSim.Keyboard.KeyPress(SelectedKeyCode);
+                timer_label.Text = "time: 0:0:0";
+                AllOk?.Play();
+                counter = 0;
+                s = 0;
+                m = 0;
+                h = 0;
+                MessageBox.Show("Time has passed", "COMPLETED", MessageBoxButtons.OKCancel, MessageBoxIcon.None);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer_is_stoped == false)
+            {
+
+                timer_is_stoped = true;
+                timer1.Stop();
+                timer1 = null;
+                seconds_mtb.Clear();
+                minutes_mtb.Clear();
+                hours_mtb.Clear();
+                Debug.Write("stop");
+                timer_label.Text = "time: 0:0:0";
+                AllOk?.Play();
+                counter = 0;
+                s = 0;
+                m = 0;
+                h = 0;
+                MessageBox.Show("Timer is stopped", "STOP", MessageBoxButtons.OKCancel, MessageBoxIcon.None);
+
+            }
+            else
+            {
+                Bad?.Play();
+                MessageBox.Show("Time is alredy stopped", "STOP", MessageBoxButtons.OKCancel, MessageBoxIcon.None);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                if (AllOk == null | Bad == null)
+                {
+                    AllOk = new SoundPlayer(Resource1.waru_ku_nai_kashira);
+                    Bad = new SoundPlayer(Resource1.ya);
+                }
+            }
+            else
+            {
+                AllOk = null;
+                Bad = null;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedKeyCode = (VirtualKeyCode)comboBox1.SelectedItem;
+        }
+    }
+}
